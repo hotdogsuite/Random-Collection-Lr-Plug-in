5d5329a2b23acd4ad9da7a95ca1a63e015b578a6
William Feldhaus <bill@Williams-MacBook-Pro.local>
2016-05-30 21:50:05 -0400

    First code

---
+++ RandomSet.lrdevplugin/Info.lua
@@ -0,0 +1,17 @@
+return {
+    LrSdkVersion = 6.0,
+    LrToolkitIdentifier = 'com.robot-wranglers.lightroom.randomset',
+    
+    LrPluginName = 'Random Set',
+    
+    LrLibraryMenuItems = {
+        title = 'Create Random Set',
+        file = 'RandomSet.lua'
+    },
+    VERSION = {
+        major = 0,
+        minor = 1,
+        revision = 0,
+        display = "0.1.00"
+    }
+}
---
+++ RandomSet.lrdevplugin/RandomSet.lua
@@ -0,0 +1,35 @@
+local LrApplication = import 'LrApplication'
+local LrTasks = import 'LrTasks'
+local LrLogger = import 'LrLogger'
+
+local myLogger = LrLogger('libraryLogger')
+myLogger:enable('print')
+
+local activeCatalog = LrApplication:activeCatalog()
+
+LrTasks.startAsyncTask(function()
+    activeCatalog:withWriteAccessDo(
+        'Create Random Set',
+        function()
+                
+            local randomSet = activeCatalog:createCollectionSet('Random Set', nil, true)
+            local randomCollection = activeCatalog:createCollection(os.date('%Y-%m-%d %H-%M-%S'), randomSet, true)
+                
+            local allPhotos = activeCatalog:getAllPhotos()
+
+Count = 0
+for Index, Value in pairs( allPhotos ) do
+  Count = Count + 1
+end
+                
+            math.randomseed(os.time())
+            for i = 1, 50, 1
+            do
+                    index = math.random(1, Count)
+                    myLogger:trace(index)
+                randomCollection:addPhotos({allPhotos[index]})
+            end
+                
+        end,
+        { timeout = 10 })
+end)
